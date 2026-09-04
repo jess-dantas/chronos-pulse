9022af3c5156da53a90648116e212d799bd7a2de
Jesse D. Lima <jess.dantas.it@gmail.com>
2026-09-03 22:19:33 -0300

    Include SecurityConfig

--- src/main/java/br/com/jess/chronos/pulse/infrastructure/config/CorsConfig.java
+++ src/main/java/br/com/jess/chronos/pulse/infrastructure/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(allowedOrigins.split(","))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
+                        .allowedHeaders("Content-Type", "Authorization")
                         .allowCredentials(true);
             }
         };
